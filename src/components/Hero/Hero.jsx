b191e8b7037919b2fc0a7b37e649f86debb743a6
Alexander Kacharava <alexander@muze.nl>
2022-12-29 14:22:40 +0100

    add hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,35 @@
+import './Hero.css'
+import logo from '../../assets/img/logo.png'
+import Button from '../Button/Button'
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-text-container">
+        <div className="hero-title">
+          <h1>Hi,</h1>
+        </div>
+        <div className="hero-title">
+          <h2>I'm Alex,</h2>
+        </div>
+        <div className="hero-title">
+          <h3>Web developer</h3>
+        </div>
+        <div className="hero-text">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            tempore est cupiditate dolorem culpa vitae soluta accusamus ab
+            animi, reprehenderit, aspernatur odit voluptate delectus architecto
+            in corporis velit ad officia.
+          </p>
+        </div>
+        <Button name="Scroll Down" />
+      </div>
+      <div className="hero-img-container">
+        <img src={logo} />
+      </div>
+    </section>
+  );
+}
+
+export default Hero
