f9728b218b3d40450ced4b6d202c967ee7efa7d6
Alexander Kacharava <alexander@muze.nl>
2023-01-10 10:43:58 +0100

    Add project component

---
+++ src/components/Projects/Project/Project.jsx
@@ -0,0 +1,12 @@
+import './Project.css'
+
+function Project() {
+  return (
+    <li className='project-item'>
+      <div className="project-content">CONTENT OF PROJECT</div>
+      <div className="project-img">PROJECT IMG</div>
+    </li>
+  );
+}
+
+export default Project
