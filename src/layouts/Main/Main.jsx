703b7c1a168d20421dca698cde0496b763f8d05c
Alexander Kacharava <alexander@muze.nl>
2023-01-10 10:44:18 +0100

    Add new components to main component

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,13 +1,18 @@
 import './Main.css'
 import Hero from '../../components/Hero/Hero'
 import About from '../../components/About/About'
+import Experience from '../../components/Experience/Experience'
+import Projects from '../../components/Projects/Projects'
+import Contact from '../../components/Contact/Contact'
 
 function Main(){
   return (
     <main>
       <Hero />
       <About />
-      <div>Section 3</div>
+      <Experience />
+      <Projects />
+      <Contact />
     </main>
   );
 }
