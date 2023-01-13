a58a1b27de3f005723a1787b38e8d3ec318b7764
Alexander Kacharava <alexander@muze.nl>
2023-01-13 16:44:58 +0100

    Adjust styling projects components

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -1,10 +1,34 @@
 import './Project.css'
+import List from '../../List/List'
 
-function Project() {
+
+function Project(props) {
   return (
-    <li className='project-item'>
-      <div className="project-content">CONTENT OF PROJECT</div>
-      <div className="project-img">PROJECT IMG</div>
+    <li className="project-item">
+      <div className="project-content">
+        <p>{props.overline}</p>
+        <h3 className="project-title">
+          <a href={props.url}>{props.title}</a>
+        </h3>
+        <div className="project-description">
+          <p>{props.description}</p>
+        </div>
+        <List className="project-tech-list" listItems={props.tech} />
+        <div className="project-links">
+          <a>Github</a>
+          <a>{props.url}</a>
+        </div>
+      </div>
+      <div className="project-img">
+        <a href={props.url}>
+          <div className="img img-wrapper img-wrapper-constrained">
+            <div>
+              <img src={props.imgSrc} />
+            </div>
+            <img src={props.imgSrc} />
+          </div>
+        </a>
+      </div>
     </li>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,27 @@
 import './Projects.css'
+import projectImg from '../../assets/img/logo.png'
 import Project from './Project/Project'
+
+const projectTechMuze = [
+  { id: 1, content: "JavaScript (ES6+)" },
+  { id: 2, content: "React" },
+  { id: 3, content: "Vue.js" },
+  { id: 3, content: "Node.js" },
+]
 
 function Projects() {
   return (
     <section className="projects">
-      <h2>Some stuff I've built</h2>
+      <h2 className="heading">Some stuff I've built</h2>
       <ul>
-        <Project />
-        <Project />
-        <Project />
+        <Project
+          overline="first"
+          url="https://brittanychiang.com/"
+          title="Test"
+          description="FOO BAAR BAZ"
+          tech={projectTechMuze}
+          imgSrc={projectImg}
+        />
       </ul>
     </section>
   );
