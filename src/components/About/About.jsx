6b251f585ff9696894bee889328f87831504f4b1
Alexander Kacharava <alexander@muze.nl>
2023-01-20 15:44:13 +0100

    edit about component styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,7 +14,7 @@ const listItems = [
 function About() {
   return (
     <section className="about">
-      <h2 className='heading'>About Me</h2>
+      <h2 className="heading">About Me</h2>
       <div className="about-inner">
         <div className="about-img-container">
           <div className="about-img-wrapper">
@@ -27,21 +27,21 @@ function About() {
         </div>
         <div className="about-text-container">
           <div>
-            <Text
-              content="Hello! My name is Alex and I enjoy creating things that live on
+            <p>
+              Hello! My name is Alex and I enjoy creating things that live on
               the internet. My interest in web development started back in 2016
               when I decided to try programming for the first time — turns out
               hacking together a custom webpage (With terrible color
-              combinations) taught me a lot about HTML & CSS!"
-            />
-            <Text
-              content="Fast-forward to today, and I’ve had the privilege of working at a
-              VR/AR agency, a start-up and a web agency with 25+ years of
+              combinations) taught me a lot about HTML & CSS!
+            </p>
+            <p>
+              Fast-forward to today, and I’ve had the privilege of working at a
+              <a>VR/AR agency</a>, a start-up and a web agency with 25+ years of
               experience. My main focus these days is building accessible,
               inclusive products and digital experiences at Muze for a variety
-              of clients."
-            />
-            <Text content="Here are a few technologies I’ve been working with recently:" />
+              of clients.
+            </p>
+            <p>Here are a few technologies I’ve been working with recently:</p>
             <List className="about-text-list" listItems={listItems} />
           </div>
         </div>
