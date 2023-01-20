3f80e203fb3c7d2f1fefbdf43248d9722357a702
Alexander Kacharava <alexander@muze.nl>
2023-01-20 14:38:30 +0100

    responsive header and about component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -22,6 +22,27 @@ function Header() {
           </ul>
           <Button className="header-button" name="Resume" />
         </div>
+        <div className="menu-nav-small">
+          <div>
+            <button className="menu-hamburger">
+              <div className="hamburger-box">
+                <div className="hamburger-box-inner"></div>
+              </div>
+            </button>
+          </div>
+          {/* <ul>
+            <li>
+              <a href="#about">about</a>
+            </li>
+            <li>
+              <a href="#projects">projects</a>
+            </li>
+            <li>
+              <a href="#contact">contact</a>
+            </li>
+          </ul>
+          <Button className="header-button" name="Resume" /> */}
+        </div>
       </nav>
     </header>
   );
