4627ab45266b2767bcbf927b82cb9b31725a81ea
Alexander Kacharava <alexander@muze.nl>
2023-03-07 13:46:04 +0100

    add new projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,26 @@
 import './Projects.css'
-import projectImg from '../../assets/img/logo.png'
 import volkorenTv from "../../assets/img/projects/volkorentv.png";
+import fuchsiaGradient from "../../assets/img/projects/fuschia-gradient.png";
+import reactMeals from "../../assets/img/projects/reactmeals.png";
 import Project from './Project/Project'
 
-const projectTechMuze = [
+const volkorenTvTech = [
+  { id: 1, content: "JavaScript (ES6+)" },
+  { id: 2, content: "WordPress" },
+  { id: 3, content: "PHP" }
+]
+
+const fuchsiaGradientTech = [
+  { id: 1, content: "JavaScript (ES6+)" },
+  { id: 3, content: "Vue.js" },
+  { id: 4, content: "Node.js" },
+]
+
+const reactMealsTech = [
   { id: 1, content: "JavaScript (ES6+)" },
   { id: 2, content: "React" },
   { id: 3, content: "Vue.js" },
-  { id: 3, content: "Node.js" },
+  { id: 4, content: "Node.js" },
 ]
 
 function Projects() {
@@ -16,32 +29,50 @@ function Projects() {
       <h2 className="heading">Some stuff I've built</h2>
       <ul>
         <Project
-          url="https://example.com/"
-          title="first"
-          tech={projectTechMuze}
-          githubUrl="https://example.com/"
+          url="https://volkoren.tv/"
+          title="VolKoren.tv"
+          tech={volkorenTvTech}
+          githubUrl=""
           imgSrc={volkorenTv}
         >
-          A platform where you can check out videos from the Dutch and Flemish
-          choral world. From classical chorus to modern acapella groups, you can
-          find it all on <a href="https://volkoren.tv/">VolKoren.tv</a>.
+          <p>
+            A platform where you can check out videos from the Dutch and Flemish
+            choral world. From classical chorus to modern acapella groups, you
+            can find it all on <a href="https://volkoren.tv/">VolKoren.tv</a>.
+          </p>
         </Project>
         <Project
-          url="https://example.com/"
-          title="second"
-          description="FOO BAAR BAZ"
-          tech={projectTechMuze}
-          githubUrl="https://example.com/"
-          imgSrc={projectImg}
-        />
+          url="https://fuchsia-gradient.akdev.nl/"
+          title="Fuchsia Gradient"
+          tech={fuchsiaGradientTech}
+          githubUrl="https://github.com/aKacharava/fuchsia-gradient"
+          imgSrc={fuchsiaGradient}
+        >
+          <p>
+            A color gradient editor where you can visually see different color
+            gradients in action. Input different colors, lock them in and change
+            the color gradient order in real time to your liking.
+          </p>
+        </Project>
         <Project
-          url="https://example.com/"
-          title="third"
-          description="FOO BAAR BAZ"
-          tech={projectTechMuze}
-          githubUrl="https://example.com/"
-          imgSrc={projectImg}
-        />
+          url="https://reactmeals.akdev.nl/"
+          title="ReactMeals"
+          tech={reactMealsTech}
+          githubUrl="https://github.com/aKacharava/react-foodapp"
+          imgSrc={reactMeals}
+        >
+          <p>
+            A mock food app that I made when I wanted to learn more about a
+            shopping cart system. You can add, edit and delete meal items to the
+            shopping cart with the amount you want and the cart will update in
+            real time.
+          </p>
+          <p>
+            After struggling with adding more of the same items in the cart
+            without duplicates in the shopping cart list, I can say that I
+            leanred a lot about React and my problem solving skills.
+          </p>
+        </Project>
       </ul>
     </section>
   );
