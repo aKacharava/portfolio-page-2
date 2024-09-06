cb559515c9a2eb6ac59ced3f9ee1bbaa42f820a2
Alexander Kacharava <alexander@cube.nl>
2024-09-06 10:44:28 +0200

    Remove ids

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -28,17 +28,14 @@ export const experiences = [
         tab: 4,
         contents: [
             {
-                id: 1,
                 content:
                     "Develop high-quality, efficient, and secure code for a variety of client and internal projects",
             },
             {
-                id: 2,
                 content:
                     "Utilize a wide range of languages, platforms, frameworks, and content management systems, including Laravel, React, Vue, SilverStripe, Statemic and Livewire",
             },
             {
-                id: 3,
                 content:
                     "Working on projects for major clients like Pure Energie and Bouwinvest, contributing to their digital strategies and helping to enhance their online presence",
             },
@@ -52,17 +49,14 @@ export const experiences = [
         tab: 3,
         contents: [
             {
-                id: 1,
                 content:
                     "Write modern, performant and maintainable code for a diverse array of client and internal projects",
             },
             {
-                id: 2,
                 content:
                     "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, React, and Vue",
             },
             {
-                id: 3,
                 content:
                     "Communicate with teams of engineers and clients on a daily basis",
             },
@@ -76,17 +70,14 @@ export const experiences = [
         tab: 2,
         contents: [
             {
-                id: 1,
                 content:
                     "Learned how to use different set of tools to create a wide variaty of products",
             },
             {
-                id: 2,
                 content:
                     "Work with a small, but experienced, team of developers that create webapps",
             },
             {
-                id: 3,
                 content:
                     "Interfaced with clients on a weekly basis, providing technological expertise",
             },
@@ -100,17 +91,14 @@ export const experiences = [
         tab: 1,
         contents: [
             {
-                id: 1,
                 content:
                     "Got an introduction to work with different kinds of branches in a company",
             },
             {
-                id: 2,
                 content:
                     "The start-up where I created the WordPress theme for VolKoren.tv",
             },
             {
-                id: 3,
                 content:
                     "Introduced a different workflow within the team",
             },
@@ -124,17 +112,14 @@ export const experiences = [
         tab: 0,
         contents: [
             {
-                id: 1,
                 content:
                     "The first internship where I experienced different types of developers",
             },
             {
-                id: 2,
                 content:
                     "Got an introduction to Agile, learned how to work with Scrum and version control",
             },
             {
-                id: 3,
                 content:
                     "Advanced my skills in HTML, CSS and JavaScript and also different concepts like classes, OOP, etc.",
             },
